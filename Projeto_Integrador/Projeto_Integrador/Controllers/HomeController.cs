5de15cfe860d0775c296683e96348253d83838cb
Daniel Peralba <contatodanielperalba@hotmail.com>
2023-09-08 19:09:06 -0300

    Implantando tela 'Sobre'
    Neste commit eu inseri a nova tela na aplicação e avancei com o design da mesma.
    
    A tela ainda está sujeita a mudanças no design, mas por enquanto o design será mantido.

--- Projeto_Integrador/Projeto_Integrador/Controllers/HomeController.cs
+++ Projeto_Integrador/Projeto_Integrador/Controllers/HomeController.cs
@@ -31,6 +31,11 @@ namespace Projeto_Integrador.Controllers
             return View();
         }
 
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
