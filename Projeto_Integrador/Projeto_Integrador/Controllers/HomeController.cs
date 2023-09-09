30f2f1183bb547bac1797f521c1ced6b5b4de692
Daniel Peralba <contatodanielperalba@hotmail.com>
2023-09-08 21:21:00 -0300

    Exclusão da aba 'Local'

--- Projeto_Integrador/Projeto_Integrador/Controllers/HomeController.cs
+++ Projeto_Integrador/Projeto_Integrador/Controllers/HomeController.cs
@@ -35,6 +35,10 @@ namespace Projeto_Integrador.Controllers
         {
             return View();
         }
+        public IActionResult Local()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
