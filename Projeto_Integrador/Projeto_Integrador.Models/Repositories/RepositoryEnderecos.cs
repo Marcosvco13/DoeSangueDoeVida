cd51f96103bee8365766a4caaa7973c1f263e117
Daniel Peralba <contatodanielperalba@hotmail.com>
2023-10-14 16:48:28 -0300

    Implementação da tela de endereços

---
+++ Projeto_Integrador/Projeto_Integrador.Models/Repositories/RepositoryEnderecos.cs
@@ -0,0 +1,16 @@
+﻿using Projeto_Integrador.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Integrador.Models.Repositories
+{
+    public class RepositoryEnderecos : RepositoryBase<CadEndereco>
+    {
+        public RepositoryEnderecos(bool saveChanges = true) :base(saveChanges) 
+        {
+        }
+    }
+}
---
+++ Projeto_Integrador/Projeto_Integrador/Controllers/EnderecosController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using Projeto_Integrador.Models.Models;
+using Projeto_Integrador.Models.Services;
+using Projeto_Integrador.ViewModel;
+
+namespace Projeto_Integrador.Controllers
+{
+    public class EnderecosController : Controller
+    {
+        private ServiceEnderecos _ServiceEnderecos;
+
+        public EnderecosController()
+        {
+            _ServiceEnderecos = new ServiceEnderecos();
+        }
+        public IActionResult Index()
+        {
+            var listarEndereco = EnderecoVM.ListarTodosEnderecos();
+            return View(listarEndereco);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create (EnderecoVM enderecoVM)
+        {
+            var endereco = new CadEndereco();
+            endereco.Id = enderecoVM.id;
+            endereco.IdUser = enderecoVM.idUser;
+            endereco.Logradouro = enderecoVM.logradouro;
+            endereco.Numero = enderecoVM.numero;
+            endereco.Complemento = enderecoVM.complemento;
+            endereco.Cep = enderecoVM.cep;
+            endereco.Bairro = enderecoVM.bairro;
+            endereco.Cidade = enderecoVM.cidade;
+            endereco.Estado = enderecoVM.estado;
+
+            await _ServiceEnderecos.oRepositoryEnderecos.IncluirAsync(endereco);
+            return View(endereco);
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> Edit (int id)
+        {
+            var endereco = await _ServiceEnderecos.oRepositoryEnderecos.SelecionarPkAsync(id);
+            return View(endereco);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CadEndereco cadEndereco)
+        {
+            if(ModelState.IsValid)
+            {
+                var endereco = await _ServiceEnderecos.oRepositoryEnderecos.AlterarAsync(cadEndereco);
+                return View(endereco);
+            }
+            ViewData["MensagemErro"] = "Ocorreu um erro";
+            return View();
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> Delete (int id)
+        {
+            await _ServiceEnderecos.oRepositoryEnderecos.ExcluirAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details (int id)
+        {
+            var endereco = EnderecoVM.SelecionarEndereco(id);
+            return View(endereco);
+        }        
+    }
+}
