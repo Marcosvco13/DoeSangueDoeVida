15cab6323d029c57f306ac9318a36750b611ee6a
Daniel Campos <contatodanielperalba@hotmail.com>
2023-11-20 10:13:51 -0300

    Alterando nomes das colunas

--- Projeto_Integrador/Projeto_Integrador/ViewModel/DataVM.cs
+++ Projeto_Integrador/Projeto_Integrador/ViewModel/DataVM.cs
@@ -6,10 +6,19 @@ namespace Projeto_Integrador.ViewModel
 {
     public class DataVM
     {
+        [Display (Name = "Codigo Data")]
         public int id { get; set; }
+
+        [Display (Name = "Data")]
         public DateTime Data { get; set; }
+
+        [Display(Name = "Local")]
         public int IDLocal { get; set; }
+
+        [Display(Name = "Data Disponivel?")]
         public int Disponivel { get; set; }
+
+        [Display(Name = "Local")]
         public string nomeLocal {  get; set; }
 
         public static DataVM SelecionarData(int id)
--- Projeto_Integrador/Projeto_Integrador/ViewModel/LocalDoacaoVM.cs
+++ Projeto_Integrador/Projeto_Integrador/ViewModel/LocalDoacaoVM.cs
@@ -1,4 +1,5 @@
 ﻿using Projeto_Integrador.Models.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Projeto_Integrador.ViewModel
 {
@@ -10,17 +11,29 @@ namespace Projeto_Integrador.ViewModel
         }
 
         #region LocalDoacao
+        [Display(Name = "Código Local")]
         public int id { get; set; }
+        [Display(Name = "Nome")]
         public string nome { get; set; }
+        [Display(Name = "E-mail")]
         public string email { get; set; }
+        [Display(Name = "Telefone")]
         public string telefone { get; set; }
+        [Display(Name = "Rua")]
         public string rua { get; set; }
+        [Display(Name = "Número")]
         public string num {  get; set; }
+        [Display(Name = "Complemento")]
         public string comple {  get; set; }
+        [Display(Name = "CEP")]
         public string cep { get; set; }
+        [Display(Name = "Bairro")]
         public string bairro { get; set; }
+        [Display(Name = "Cidade")]
         public string cidade { get; set; }
+        [Display(Name = "Estado")]
         public string estado { get; set; }
+        [Display(Name = "Local Ativo?")]
         public int ativo { get; set; }
         #endregion
 
